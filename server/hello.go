528795f322db30618e6a7bf4e3bf6d2e83a60566
heping2018 <g246897@163.com>
2024-04-17 21:41:12 +0800

    proto

---
+++ server/hello.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	trippb "coolcar/proto/gen/go"
+	"encoding/json"
+	"fmt"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	trip := trippb.Trip{
+		Start:       "abc",
+		End:         "def",
+		DurationSec: 3600,
+		FeeCent:     10000,
+		StartPos: &trippb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		EndPos: &trippb.Location{
+			Latitude:  35,
+			Longitude: 115,
+		},
+		PathLocations: []*trippb.Location{
+			{
+				Latitude:  31,
+				Longitude: 119,
+			},
+			{
+				Latitude:  32,
+				Longitude: 118,
+			},
+		},
+		Status: trippb.TripStatus_FINISHED,
+	}
+	fmt.Println("hello")
+	fmt.Println(&trip)
+	b, err := proto.Marshal(&trip) // 序列化
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%X\n", b)
+	var trip2 trippb.Trip
+	err = proto.Unmarshal(b, &trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(&trip2)
+	b, err = json.Marshal(&trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", b)
+	var trip3 trippb.Trip
+	err = json.Unmarshal(b, &trip3)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(&trip3)
+}
